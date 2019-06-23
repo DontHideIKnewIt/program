5558ea83084179ac3e8fbab86e0047b1430ec2be
doubleloopcoding <billy.djunaidi@gmail.com>
2019-06-23 22:57:56 +0700

    first commit

---
+++ sw.js
@@ -0,0 +1,42 @@
+
+var cacheName = 'hello-world-page';
+var CACHE_STATIC_NAME = 'static-v10';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
+var filesToCache = [
+  '/index.html'
+];
+
+
+
+self.addEventListener('install', (event) =>  {
+  event.waitUntil(async function() {
+    const cache = await caches.open('static-v1');
+    await cache.addAll(['index.html','lingkaran.png','persegi.png','ppanjang.png','segitiga.png']);
+  }());
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  // Always bypass for range requests, due to browser bugs
+  if (request.headers.has('range')) return;
+  event.respondWith(async function() {
+    // Try to get from the cache:
+    const cachedResponse = await caches.match(request);
+    if (cachedResponse) return cachedResponse;
+
+    try {
+      // Otherwise, get from the network
+      return await fetch(request);
+    } catch (err) {
+      // If this was a navigation, show the offline page:
+      if (request.mode === 'navigate') {
+        return caches.match('offline.html');
+      }
+
+      // Otherwise throw
+      throw err;
+    }
+  }());
+});
